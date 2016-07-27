895109377045bd7cd83cac8f1ff84232b47ad030
jinpingwang <wangjinping2014@qq.com>
2016-07-27 09:20:39 +0800

    modify InsertPhoto

--- src/test/java/test/InsertPhoto.java
+++ src/test/java/test/InsertPhoto.java
@@ -11,7 +11,7 @@ public class InsertPhoto {
 	public static void main(String[] args) throws SQLException, Exception {
 		// TODO Auto-generated method stub
 		Photo photo = new Photo();
-		photo.addPhoto("下沙", "", "2012-04-16", "4", "/photo/image/2012/下沙/", Paths.get("H:\\xp\\游记\\下沙\\big"));
+		photo.addPhoto("马拉松", "", "2016-03-30", "4", "/photo/image/2016/马拉松/", Paths.get("D:\\相片\\马拉松"));
 		PhotosBean photosBean = photo.getPhoto(1, 1);
 		System.out.println(photosBean.getData().size());
 	}
